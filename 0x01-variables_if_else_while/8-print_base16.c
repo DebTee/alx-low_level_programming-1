f5c9e2d99575b49f45d4aca2c2cf3e7a22506a75
Danimoh Davidking LIFE <76137172+Danimohlife@users.noreply.github.com>
2022-07-03 12:25:24 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-	unsigned char a = '0';
-	int i;
-	for (i = 0; i < 10; i++)
-	{
-		purchar(a);
-		a++;
-	}
-	a = 'i';
-	for(a = 0; a < 6; a++)
-	{
-		purchar('0' + a);
-		a++;
-	}
-	purchar("/n");
-	return (0);
+unsigned char a = '0';
+int i;
+for (i = 0; i < 10; i++)
+{
+purchar(a);
+a++;
+}
+a = '1';
+for(a = 0; a < 6; a++)
+{
+purchar('0' + a);
+a++;
+}
+purchar("/n");
+return (0);
 }
