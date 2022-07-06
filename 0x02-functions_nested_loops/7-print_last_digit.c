06a5b0ba5e0852dff5a8769908375d835f47b16e
Danimoh Davidking LIFE <76137172+Danimohlife@users.noreply.github.com>
2022-07-06 05:33:13 +0100

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *@n: number being tested
+ * Return: Always 0.
+ */
+
+int print_last_digit(int)
+{
+	int ln = n % 10;
+
+	if (n < 0)
+		ln = ln* -1;
+		_putchar(ln + '0');
+		return (ln);
+}
